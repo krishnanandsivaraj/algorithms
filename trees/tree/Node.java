7e4d2923ee641b1159ef35a3fe4baf07d7654188
ksivaraj <ksivaraj@USBLRKSIVARAJ1>
2018-11-02 11:05:22 +0530

    adding a basic tree DS

---
+++ trees/tree/Node.java
@@ -0,0 +1,14 @@
+package tree;
+
+public class Node {
+	
+	int key;
+	Node left,right;
+	
+	Node(int item)
+	{
+		this.key=item;
+		left=right=null;
+	}
+
+}
---
+++ trees/tree/start.java
@@ -0,0 +1,10 @@
+package tree;
+
+public class start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
